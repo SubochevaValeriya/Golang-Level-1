2ab685750c25bc7edaab33b183953003548b899f
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-06-29 00:16:22 +0300

    added h/w lesson6

--- lesson6/main.go
+++ lesson6/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 )
 
 type myType []byte
 
-func (m *myType) Read(p []byte) (n int, err error) {
-
-	for i, value := range *m {
-		p[i] = value
+func (m myType) Read(p []byte) (n int, err error) {
+	for range p {
+		n++
 	}
 
-	n = len(*m)
 	return n, io.EOF
 }
 
-func (m *myType) Write(p []byte) (n int, err error) {
-	*m = p
-
-	n = len(*m)
-
+func (m myType) Write(p []byte) (n int, err error) {
+	for range p {
+		n++
+	}
+	if n != len(string(p)) {
+		err = errors.New("Ошибка считывания")
+	}
 	return n, err
 }
 
@@ -36,11 +37,10 @@ type Reader interface {
 func main() {
 	m := myType{}
 
-	io.WriteString(&m, "Привет")
+	io.WriteString(m, "Привет")
+	fmt.Println(m)
 
-	fmt.Println(string(m))
-
-	myStr, _ := io.ReadAll(&m)
+	myStr, _ := io.ReadAll(m)
 
 	fmt.Println(string(myStr))
 }
