e98b6eb870eeb16e9a0bd5cd28662542479934a2
ValeriyaSubocheva <subocheva.valeria@gmail.com>
2022-07-05 01:09:51 +0300

    added h/w lesson8

--- lesson8/anotherPackage/package.go
+++ lesson8/anotherPackage/package.go
@@ -2,19 +2,22 @@ package another
 
 import (
 	"flag"
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"log"
 	"net/url"
 )
 
 type MyURL url.URL
 
 type Config struct {
-	Port         string
-	Db_url       MyURL
-	Jaeger_url   MyURL
-	Sentry_url   MyURL
-	Kafka_broker string
-	Some_app_id  string
-	Some_app_key string
+	Port        string `default:"8080" envconfig:"Port"`
+	DbUrl       MyURL  `default:"postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable" envconfig:"Db_url"`
+	JaegerUrl   MyURL  `default:"http://jaeger:16686" envconfig:"jaeger_url"`
+	SentryUrl   MyURL  `default:"http://sentry:9000" envconfig:"sentry_url"`
+	KafkaBroker string `default:"kafka:9092" envconfig:"kafka_broker"`
+	SomeAppId   string `default:"testid" envconfig:"some_app_id"`
+	SomeAppKey  string `default:"testkey" envconfig:"some_app_key"`
 }
 
 func (u *MyURL) Decode(value string) error {
@@ -31,22 +34,40 @@ type Decoder interface {
 	Decode(value string) error
 }
 
-func GetConfiguration() Config {
+func GetConfigurationFromFlags() Config {
 	var configuration Config
 	var dbUrl, jaegerUrl, sentryUrl string
 
 	flag.StringVar(&configuration.Port, "port", "8080", "port to connect to DB")
 	flag.StringVar(&dbUrl, "db_url", "postgres://db-user:db-password@petstore-db:5432/petstore?sslmode=disable", "DB url")
-	configuration.Db_url.Decode(dbUrl)
+	configuration.DbUrl.Decode(dbUrl)
 	flag.StringVar(&jaegerUrl, "jaeger_url", "http://jaeger:16686", "port to connect to DB")
-	configuration.Jaeger_url.Decode(jaegerUrl)
+	configuration.JaegerUrl.Decode(jaegerUrl)
 	flag.StringVar(&sentryUrl, "sentry_url", "http://sentry:9000", "port to connect to DB")
-	configuration.Jaeger_url.Decode(sentryUrl)
-	flag.StringVar(&configuration.Kafka_broker, "kafka_broker", "kafka:9092", "port to connect to DB")
-	flag.StringVar(&configuration.Some_app_id, "some_app_id", "testid", "port to connect to DB")
-	flag.StringVar(&configuration.Some_app_key, "some_app_key", "testkey", "app_key to connect to DB")
+	configuration.JaegerUrl.Decode(sentryUrl)
+	flag.StringVar(&configuration.KafkaBroker, "kafka_broker", "kafka:9092", "port to connect to DB")
+	flag.StringVar(&configuration.SomeAppId, "some_app_id", "testid", "port to connect to DB")
+	flag.StringVar(&configuration.SomeAppKey, "some_app_key", "testkey", "app_key to connect to DB")
 
 	flag.Parse()
+
+	return configuration
+}
+
+func GetConfigurationFromEnv() Config {
+	var configuration Config
+
+	err := envconfig.Process("myapp", &configuration)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	format := "Port: %d\nDbUrl: %s\nJaegerUrl: %f\nSentryUrl: %s\nKafkaBroker: %s\nSomeAppId: %s\nSomeAppKey: %s\n"
+	_, err = fmt.Printf(format, configuration.Port, configuration.DbUrl, configuration.JaegerUrl, configuration.SentryUrl, configuration.KafkaBroker, configuration.SomeAppId, configuration.SomeAppKey)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 	return configuration
 }
--- lesson8/main.go
+++ lesson8/main.go
@@ -9,6 +9,7 @@ func main() {
 
 	// go run main.go --Port= --Db_url= -Jaeger_url= --MyURL= --Sentry_url= --Kafka_broker= --Some_app_id= --Some_app_key=
 
-	fmt.Println(another.GetConfiguration())
+	fmt.Println(another.GetConfigurationFromFlags())
+	fmt.Println(another.GetConfigurationFromEnv())
 
 }
